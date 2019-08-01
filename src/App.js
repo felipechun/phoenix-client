a4725b4a92cc1ae5b70991f6070b89de30f50bc0
Felipe Chun <felipe.ychun@gmail.com>
2019-08-01 11:50:37 -0300

    added Navbar and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, World</h1>
-    </div>
-  );
+
+class App extends Component {
+
+  // state = {
+
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        <Switch>
+          <Route component={Home} />
+        </Switch>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
