cfce82e05d3b8674484593bc6712be6061c4a962
David Dornelles <daviddornelles@gmail.com>
2019-08-05 10:55:55 -0300

    profile and status timeline

--- src/components/SellForm/SellForm.js
+++ src/components/SellForm/SellForm.js
@@ -1,51 +1,54 @@
-import React, { Component } from 'react'
-import AuthService from '../authentication/auth-service/auth-service';
+import React, { Component } from "react";
+import AuthService from "../authentication/auth-service/auth-service";
+import Label from "../label/Label";
+import Input from "../input/Input";
+import SubmitBtn from "../buttons/SubmitBtn";
 
 export class SellForm extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      statusProduct: '',
-      categories: '',
-      path: '',
-      brand: '',
-      model: '',
-      starterPrice: '',
-      clientDescription: '',
-      imageUrl: '', 
-      };
+      path: "",
+      categories: "",
+      brand: "",
+      name: "",
+      model: "",
+      statusProduct: "",
+      starterPrice: "",
+      clientDescription: "",
+      imageUrl: ""
+    };
     this.service = new AuthService();
   }
 
-  handleFormSubmit = (event) => {
+  handleFormSubmit = event => {
     event.preventDefault();
     const username = this.state.username;
     const password = this.state.password;
-  
-    this.service.signup(username, password)
-    .then( response => {
+
+    this.service
+      .signup(username, password)
+      .then(response => {
         this.setState({
-          name: '',
-          statusProduct: '',
-          categories: '',
-          path: '',
-          brand: '',
-          model: '',
-          starterPrice: '',
-          clientDescription: '',
-          imageUrl: '', 
+          path: "",
+          categories: "",
+          brand: "",
+          name: "",
+          model: "",
+          statusProduct: "",
+          starterPrice: "",
+          clientDescription: "",
+          imageUrl: ""
         });
-        this.props.getUser(response)
-    })
-    .catch( error => console.log(error) )
-  }
-  
-  handleChange = (event) => {  
-    const {name, value} = event.target;
-    this.setState({[name]: value});
-  }
+        this.props.getUser(response);
+      })
+      .catch(error => console.log(error));
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   handleFileUpload = e => {
     console.log("The file to be uploaded is: ", e.target.files[0]);
@@ -53,80 +56,201 @@ export class SellForm extends Component {
     const uploadData = new FormData();
     // imageUrl => this name has to be the same as in the model since we pass
     uploadData.append("imageUrl", e.target.files[0]);
-    
-    this.service.handleUpload(uploadData)
-    .then(response => {
-        // after the console.log we can see that response carries 'secure_url' which we can use to update the state 
-        this.setState({ imageUrl: response.secure_url }, () => console.log('IMAGE', this.state.imageUrl));
+
+    this.service
+      .handleUpload(uploadData)
+      .then(response => {
+        // after the console.log we can see that response carries 'secure_url' which we can use to update the state
+        this.setState({ imageUrl: response.secure_url }, () =>
+          console.log("IMAGE", this.state.imageUrl)
+        );
       })
       .catch(err => {
         console.log("Error while uploading the file: ", err);
       });
-}
+  };
 
   render() {
     console.log(this.state.path);
     return (
-      <div className="container">
-        <form onSubmit={this.handleFormSubmit} encType="multipart/form-data">
-        <div className="form-group">
-          <label>What do you want to do?</label>
-            <select name="path" required onChange={ e => this.handleChange(e)} className="form-control" >
-              <option value="" >Choose one</option>
-              <option value="Repair">Repair</option>
-              <option value="Sell">Sell</option>
-            </select>
-            <label>Category</label>
-            <select name="categories" required onChange={ e => this.handleChange(e)} className="form-control" >
-              <option value="" >Choose one</option>
-              <option value="Laptop">Laptop</option>
-              <option value="Tablet">Tablet</option>
-              <option value="Mobile">Mobile</option>
-              <option value="Consoles">Videogame Console</option>
-              <option value="TV's">Television</option>
-              <option value="Audio">Audio</option>
-            </select>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-12">
+            <h4 className="border-bottom text-uppercase">
+              Product Register Form
+            </h4>
           </div>
-          <div className="form-group">
-            <label>Product Brand</label>
-            <input type="text" name="brand" className="form-control" value={this.state.brand} required onChange={ e => this.handleChange(e)} placeholder="Brand of the product"  />
-          </div>
-          <div className="form-group">
-            <label>Product</label>
-            <input type="text" name="name" className="form-control" value={this.state.name} required onChange={ e => this.handleChange(e)} placeholder="Name of the product"  />
-          </div>
-          <div className="form-group">
-            <label>Model</label>
-            <input type="text" name="model" className="form-control" value={this.state.model} required onChange={ e => this.handleChange(e)} placeholder="Brand of the product"  />
-          </div>
-          <div className="form-group">
-            <label>Condition</label>
-            <select name="statusProduct" required onChange={ e => this.handleChange(e)} className="form-control" >
-              <option value="" >Choose one</option>
-              <option value="Broken">Broken</option>
-              <option value="Semi-used">Semi-used</option>
-            </select>
-          </div>
-          {
-            this.state.path === 'Sell' ? (
+        </div>
+        <form
+          className="w-100"
+          onSubmit={this.handleFormSubmit}
+          encType="multipart/form-data"
+        >
+          <div className="row">
+            <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-3">
               <div className="form-group">
-                <label>Suggested Price</label>
-                <input type="text" name="starterPrice" className="form-control" value={this.state.starterPrice} required onChange={ e => this.handleChange(e)} placeholder="How much do you think your product is worth?"  />
+                <Label labelFor="service">What do you want to do?</Label>
+                <select
+                  id="service"
+                  name="path"
+                  required
+                  onChange={e => this.handleChange(e)}
+                  className="form-control"
+                >
+                  <option value="">Choose one</option>
+                  <option value="Repair">Repair</option>
+                  <option value="Sell">Sell</option>
+                </select>
               </div>
-            ) : null
-          }
-          <div className="form-group">
-            <label >Description</label>
-            <textarea className="form-control" name="clientDescription" value={this.state.clientDescription} required onChange={ e => this.handleChange(e)} placeholder="Does your device have any problems or physical damages such as a broken screen or scuffs? Please be as specific as possible." rows="3"></textarea>
-          </div>
-          <div className="form-group">
-            <label>Please upload a picture of your product</label>
-            <input type="file" className="form-control-file" required onChange={ e => this.handleFileUpload(e)}/>
+            </div>
+            <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-3">
+              <div className="form-group">
+                <Label labelFor="category">Category</Label>
+                <select
+                  id="category"
+                  name="categories"
+                  required
+                  onChange={e => this.handleChange(e)}
+                  className="form-control"
+                >
+                  <option value="">Choose one</option>
+                  <option value="Laptop">Laptop</option>
+                  <option value="Tablet">Tablet</option>
+                  <option value="Mobile">Mobile</option>
+                  <option value="Consoles">Videogame Console</option>
+                  <option value="TV's">Television</option>
+                  <option value="Audio">Audio</option>
+                </select>
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-3">
+              <div className="form-group">
+                <Label labelFor="brand">Product Brand</Label>
+                <Input
+                  id="brand"
+                  type="text"
+                  name="brand"
+                  className="form-control"
+                  value={this.state.brand}
+                  onChange={e => this.handleChange(e)}
+                  placeholder="Brand of the product"
+                  required
+                />
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-3">
+              <div className="form-group">
+                <Label labelFor="product">Product</Label>
+                <Input
+                  id="product"
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  value={this.state.name}
+                  onChange={e => this.handleChange(e)}
+                  placeholder="Name of the product"
+                  required
+                />
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-3">
+              <div className="form-group">
+                <Label labelFor="model">Model</Label>
+                <Input
+                  id="model"
+                  type="text"
+                  name="model"
+                  className="form-control"
+                  value={this.state.model}
+                  onChange={e => this.handleChange(e)}
+                  placeholder="Model of the product"
+                  required
+                />
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-3">
+              <div className="form-group">
+                <Label labelFor="condition">Condition</Label>
+                <select
+                  id="condition"
+                  name="statusProduct"
+                  onChange={e => this.handleChange(e)}
+                  className="form-control"
+                  required
+                >
+                  <option value="">Choose one</option>
+                  <option value="Broken">Broken</option>
+                  <option value="Semi-used">Semi-used</option>
+                </select>
+              </div>
+            </div>
+            {this.state.path === "Sell" ? (
+              <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-6">
+                <div className="form-group">
+                  <Label labelFor="suggested">Suggested Price</Label>
+                  <Input
+                    id="suggested"
+                    type="text"
+                    name="starterPrice"
+                    className="form-control"
+                    value={this.state.starterPrice}
+                    onChange={e => this.handleChange(e)}
+                    placeholder="How much do you think your product is worth?"
+                    required
+                  />
+                </div>
+              </div>
+            ) : null}
+            <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-12">
+              <div className="form-group">
+                <Label labelFor="description">Description</Label>
+                <textarea
+                  id="description"
+                  className="form-control"
+                  name="clientDescription"
+                  value={this.state.clientDescription}
+                  onChange={e => this.handleChange(e)}
+                  placeholder="Does your device have any problems or physical damages such as a broken screen or scuffs? Please be as specific as possible."
+                  rows="2"
+                  required
+                />
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-12">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="upload">
+                    Upload
+                  </span>
+                </div>
+                <div className="custom-file">
+                  <Input
+                    id="file"
+                    type="file"
+                    className="custom-file-input"
+                    value={this.state.imageUrl}
+                    onChange={e => this.handleFileUpload(e)}
+                    aria-describedby="upload"
+                    required
+                  />
+                  <label className="custom-file-label" htmlFor="file">
+                    Please upload a picture of your product
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 ">
+              <div className="d-flex flex-column align-items-center">
+                <SubmitBtn type="submit" className="btn btn-outline-success">
+                  Submit
+                </SubmitBtn>
+              </div>
+            </div>
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default SellForm
+export default SellForm;
--- src/components/authentication/Login/Login.js
+++ src/components/authentication/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AuthService from '../auth-service/auth-service';
 import { Link, Redirect } from 'react-router-dom';
+import Label from '../../label/Label';
 import Input from '../../input/Input';
 import SubmitBtn from '../../buttons/SubmitBtn';
 
@@ -22,26 +23,47 @@ class Login extends Component {
     })
     .catch( error => console.log(error) )
   }
-    
-  handleChange = (event) => {  
+
+  handleChange = (event) => {
     const {name, value} = event.target;
     this.setState({[name]: value});
   }
-    
+
   render(){
     if (this.state.redirect) {
       return <Redirect to="/"></Redirect>
     } else {
       return(
-        <div>
-          <form onSubmit={this.handleFormSubmit}>
-            <Input placeholder="Email" type="email" name="username" value={this.state.username} required onChange={ e => this.handleChange(e)} />
-            <Input placeholder="Password" type="password" name="password" value={this.state.password} min="8" required onChange={ e => this.handleChange(e)} />
-            <SubmitBtn type="submit" className="btn btn-outline-success">Log in</SubmitBtn>
+        <div className="container mt-3">
+          <div className="row">
+            <div className="col-12">
+              <h4 className="border-bottom text-uppercase">Login</h4>
+            </div>
+          </div>
+          <form className="w-100" onSubmit={this.handleFormSubmit}>
+            <div className="row">
+              <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
+                <div className="form-group">
+                  <Label labelFor="email">Email</Label>
+                  <Input id="email" className="form-control" placeholder="Email" type="email" name="username" value={this.state.username} required onChange={ e => this.handleChange(e)} />
+                </div>
+              </div>
+              <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
+                <div className="form-group">
+                  <Label labelFor="password">Password</Label>
+                  <Input id="password" className="form-control" placeholder="Password" type="password" name="password" value={this.state.password} min="8" required onChange={ e => this.handleChange(e)} />
+                </div>
+              </div>
+              <div className="col-12 col-sm-12 col-md-10 offset-1 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2 ">
+                <div className="d-flex flex-column align-items-center">
+                  <SubmitBtn type="submit" className="btn btn-outline-success">Log in</SubmitBtn>
+                  <p className="mt-5">Don't have an account? 
+                      <Link to={"/signup"}> Signup</Link>
+                  </p>
+                </div>
+              </div>
+            </div>
           </form>
-          <p>Don't have an account? 
-              <Link to={"/signup"}> Signup</Link>
-          </p>
         </div>
       )
     }
